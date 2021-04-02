384fde815a5f44dd70e8a5c340c5261357b5ca4b
Jorge Ktch <jorgektch@gmail.com>
2021-04-02 18:45:51 -0500

    Temporal solution

---
+++ 2021/Online-Qualification-2021/ejecutar_solucion.bat
@@ -0,0 +1,7 @@
+g++ solution.cpp -o solution
+solution < a.txt > out_a.txt
+solution < b.txt > out_b.txt
+solution < c.txt > out_c.txt
+solution < d.txt > out_d.txt
+solution < e.txt > out_e.txt
+solution < f.txt > out_f.txt
--- 2021/Online-Qualification-2021/solution.cpp
+++ 2021/Online-Qualification-2021/solution.cpp
@@ -1,5 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
+// Class of the node
+class Node {
+public:
+    int vertexNumber;
+    vector<pair<int, int>> children;
+    Node(int vertexNumber){
+        this->vertexNumber = vertexNumber;
+    }
+    void add_child(int vNumber, int length){
+        pair<int, int> p;
+        p.first = vNumber;
+        p.second = length;
+        children.push_back(p);
+    }
+};
 int main(){
+    int d; cin >> d; // Simulation duration
+    int i; cin >> i; // Intersections
+    int s; cin >> s; // Number of streets
+    int v; cin >> v; // Number of cars
+    int f; cin >> f; // Bonus
+    map<string, vector<int>> streets_b_e_l;
+    map<int, vector<int>> insersections;
+    for(int street=0; i<s; i++){
+        int b, e; cin >> b, e;
+        string name; cin >> name;
+        int l; cin >> l;
+        vector<int> vector_b_e_l = {b,e,l};
+        streets_b_e_l[name] = vector_b_e_l;
+        vector<int> intersection;
+        insersections[e].push_back(b);
+    }
+    
+    /*
+    for(int vehicle=0; vehicle<v; vehicle++){
+        int p; cin >> p; // Number of streets for the vehicle
+        for(int j=0; j<p; j++){
+
+        }
+    }
+    */
+    for (auto it = insersections.begin(); it != insersections.end(); it++){
+        cout << it->first << "\n";
+        for (int i=0; i<=it->second.size(); i++){
+            cout << element.size()
+            cout << element << " " << 2 << "\n";
+        }
+    }
     return 0;
 }
